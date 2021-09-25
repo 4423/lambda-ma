ffbdc6725ad44daa14861940f1fdad27a1b71702
4423 <hanamiya4423@gmail.com>
2021-09-25 10:45:16 +0900

    Add python script

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-# prepare result file
-RESULT_FILE=result.txt
-rm $RESULT_FILE
-echo -n '' > $RESULT_FILE
-# 1. num of functor apllication
-# 2. time for code generation [s]
-# 3. time for runnning code [s]
-# 4. memory usage [KB]
-
 filesize () {
     size=$(wc -c < $1)
     echo $size '1000.0' | awk '{printf "%f", $1 / $2}'
 }
 
-for i in `seq 0 1`
+for n in `seq 0 10`
 do
+    # prepare result file
+    RESULT_FILE=result$n.txt
+    rm $RESULT_FILE
+    echo -n '' > $RESULT_FILE
+    # 1. num of functor apllication
+    # 2. time for code generation [s]
+    # 3. time for runnning code [s]
+    # 4. memory usage [KB]
+
     OUT_FILE=fix_functor.mcod.out.ml
     # add measurement code
     echo 'let t1 = Sys.time () ;;' > $OUT_FILE
@@ -28,14 +28,21 @@ do
     echo ';;\nPrintf.printf "%f\\n" (Sys.time () -. t1);;' >> $OUT_FILE
     # compile translated code
     make mcod
-    # run bench
-    echo $i | tr '\n' '\t' >> $RESULT_FILE
-    /usr/bin/time -f '%M' ./bench.out 2>&1 | tr '\n' '\t' >> $RESULT_FILE
-    # measure size of code
-    ./print_code.out | sed -e '1d' > code.dump
-    filesize code.dump >> $RESULT_FILE
-    rm code.dump
-    echo '' >> $RESULT_FILE
+
+    for i in `seq 0 100`
+    do
+        echo $n "-" $i
+        
+        # run bench
+        echo $i | tr '\n' '\t' >> $RESULT_FILE
+        /usr/bin/time -f '%M' ./bench.out 2>&1 | tr '\n' '\t' >> $RESULT_FILE
+        # measure size of code
+        ./print_code.out | sed -e '1d' > code.dump
+        filesize code.dump >> $RESULT_FILE
+        rm code.dump
+        echo '' >> $RESULT_FILE
+    done
+
     # clean
     make clean
     rm $OUT_FILE
