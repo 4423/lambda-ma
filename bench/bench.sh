7aff4921e67a56f9d44baaff94461b028213056d
4423 <hanamiya4423@gmail.com>
2021-09-25 10:56:57 +0900

    Fix bench

--- bench/bench.sh
+++ bench/bench.sh
@@ -16,23 +16,22 @@ do
     # 3. time for runnning code [s]
     # 4. memory usage [KB]
 
-    OUT_FILE=fix_functor.mcod.out.ml
-    # add measurement code
-    echo 'let t1 = Sys.time () ;;' > $OUT_FILE
-    # translate
-    OUT_TMP_FILE=fix_functor.mcod.out.tmp.ml
-    cat fix_functor.mcod.ml | sed s/NUM_ITERATION/$i/g > $OUT_TMP_FILE
-    ../src/lambda-ma $OUT_TMP_FILE >> $OUT_FILE
-    rm $OUT_TMP_FILE
-    # add measurement code
-    echo ';;\nPrintf.printf "%f\\n" (Sys.time () -. t1);;' >> $OUT_FILE
-    # compile translated code
-    make mcod
-
     for i in `seq 0 100`
     do
         echo $n "-" $i
-        
+
+        OUT_FILE=fix_functor.mcod.out.ml
+        # add measurement code
+        echo 'let t1 = Sys.time () ;;' > $OUT_FILE
+        # translate
+        OUT_TMP_FILE=fix_functor.mcod.out.tmp.ml
+        cat fix_functor.mcod.ml | sed s/NUM_ITERATION/$i/g > $OUT_TMP_FILE
+        ../src/lambda-ma $OUT_TMP_FILE >> $OUT_FILE
+        rm $OUT_TMP_FILE
+        # add measurement code
+        echo ';;\nPrintf.printf "%f\\n" (Sys.time () -. t1);;' >> $OUT_FILE
+        # compile translated code
+        make mcod
         # run bench
         echo $i | tr '\n' '\t' >> $RESULT_FILE
         /usr/bin/time -f '%M' ./bench.out 2>&1 | tr '\n' '\t' >> $RESULT_FILE
@@ -41,9 +40,8 @@ do
         filesize code.dump >> $RESULT_FILE
         rm code.dump
         echo '' >> $RESULT_FILE
+        # clean
+        make clean
+        rm $OUT_FILE
     done
-
-    # clean
-    make clean
-    rm $OUT_FILE
 done
